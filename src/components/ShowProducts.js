66df50e946db0519cb28f8e0107c579d6a421b54
Kevin Arce <iav.kevinarce@ufg.edu.sv>
2022-05-25 11:41:10 -0600

    reactfront

---
+++ src/components/ShowProducts.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react'
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
+
+
+const ShowProducts = () => {
+    const [ products, setProducts ] = useState([])
+
+    const [ count, setCount ] = useState([])
+
+    
+    const getAllProducts = ()  => {
+    }
+
+    const deleteProduct = () => {
+    }
+
+
+  return (
+    <div>ShowProducts</div>
+  )
+}
+
+export default ShowProducts
